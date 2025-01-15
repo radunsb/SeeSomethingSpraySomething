eac959656c563c1edf553a5fcd76e4f484460cd6
radunsb <93169946+radunsb@users.noreply.github.com>
2025-01-14 20:47:47 -0500

    Getting Parameters in usable state
    Single drawer is now populated with parameters, but changing them doesn't do anything yet

--- SeeSomethingSpraySomething/src/App.tsx
+++ SeeSomethingSpraySomething/src/App.tsx
@@ -2,17 +2,39 @@ import React from 'react';
 import './App.css';
 import { Drawer } from './Drawers';
 import { NavLink, Link } from "react-router";
-import MainScreenVisual from './mainScreenVisual';
+import {createProjectMap} from './ProjectUtilities';
+import { useState, useEffect } from "react";
 
 export default function App() {
   const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
-
+  const [parameterMap, setParameterMap] = useState(new Map());
+  //Immediately loads the first project of the first user(default project)
+  //sets the parameter map to it
+  useEffect(() => {
+    async function loadDefaultProject(){
+      setParameterMap(await createProjectMap(1));
+    }
+    loadDefaultProject();
+  })
+  //TODO: make the parameterList also controlled by state so it can update
+  //Constructs a list of the parameters and their default values
+  const parameterList = [];
+    for(const [key, value] of parameterMap){
+      parameterList.push(
+        <li id={key + "_list"} key={key}>
+          <p>{key}</p>
+          <input id={key + "_input"} type="text" defaultValue={value}></input>
+        </li>
+      )
+    }
+  
   return (
     <div>
       <button onClick={() => setIsDrawerOpen(true)}>Open Drawer</button>
 
       <Drawer isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)}>
         <p>Drawer</p>
+        <ul>{parameterList}</ul>
       </Drawer>
       <div>
           <Link to="/results">
@@ -21,9 +43,6 @@ export default function App() {
           <Link to="/parameters">
             <button> Parameters </button>
           </Link>
-      </div>
-      <div>
-        <MainScreenVisual/>
       </div>
     </div>
   );
---
+++ SeeSomethingSpraySomething/src/ProjectUtilities.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import {Models} from './models.ts'
+
+//Async function that constructs a map with project data
+//TODO: This currently just takes the user's first project.
+//Should make version with a projectID as well
+export async function createProjectMap(userID: number){
+    let user: Partial<Models.User> = {};
+    await axios.get(`http://localhost:5000/api/v1/users/${userID}`)
+        .then(response => {
+            user = <Models.User> response.data.user;
+        })
+        .catch(error => console.error(error));
+    const parameterMap = new Map();
+    if(user.projects){
+        const projectToFetch = 0;
+        const project = user.projects[projectToFetch];        
+        Object.entries(project).map(entry => {
+            const key = entry[0]
+            const value = entry[1]
+            parameterMap.set(key, value)
+        })
+        const nozzle = project.nozzle;
+        Object.entries(nozzle).map(entry => {
+            const key = entry[0]
+            const value = entry[1]
+            parameterMap.set(key, value)
+        })
+        const controller = project.controller;
+        Object.entries(controller).map(entry => {
+            const key = entry[0]
+            const value = entry[1]
+            parameterMap.set(key, value)
+        })
+        const gun = project.gun;
+        Object.entries(gun).map(entry => {
+            const key = entry[0]
+            const value = entry[1]
+            parameterMap.set(key, value)
+        })
+        parameterMap.delete('controller');
+        parameterMap.delete('nozzle');
+        parameterMap.delete('gun');
+    }
+    return parameterMap;
+}
