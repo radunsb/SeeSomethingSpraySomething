c7f9ccedac6fd1147b02478a0d092079610771fa
hemmerlinaj20 <100312616+hemmerlinaj20@users.noreply.github.com>
2025-01-22 18:09:55 -0500

    created basic 3d model (learning react-three-fiber)

--- src/MainScreenVisual.tsx
+++ src/MainScreenVisual.tsx
@@ -1,29 +1,45 @@
 import React, { useState } from 'react';
-//import Project from './Project.tsx'; // Import the Project component
-//import Models from './utility/models.ts';
-//import {createProjectMap} from './utility/ProjectUtilities.ts';
-import changeParameterList from './App.tsx';
+import { UtilityInterfaces } from "./utility/models"
+import {Canvas} from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
 
-const MainScreenVisual = (parameterMap: Map<string, any>, setParameterMap: Function) => {
+const Cube = ({position, size, color}) => {
+    return (
+      <mesh position={position}>
+        <boxGeometry args={size}/>
+        <meshStandardMaterial color={color}/>
+      </mesh>
+    );
+}
 
-  const [inputValue, setInputValue] = useState('');
+const MainScreenVisual = (parameterMap: Map<string, UtilityInterfaces.Parameter>, setParameterMap: Function) => {
 
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>){
-    setInputValue(e.target.value);
-    //parameterMap.set(e.target.name, e.target.value);
-    setParameterMap(parameterMap.set(e.target.name, e.target.value));
-    changeParameterList
-  }
+  // TODO: Get all of the valus from the current model that we need to build the visual
+  const line_speed = parameterMap.get('line_speed').value;
+  const line_width = parameterMap.get('line_width').value;
+  const nozzle_height = parameterMap.get('nozzle_height').value;
+  const spray_angle = parameterMap.get('angle').value;
+
+  // console.log(spray_angle);
 
   return (
     <div>
       <div>
-        <h2>Project Details</h2>
-        {parameterMap.get("line_speed")}
-        {/*{parameterMap.get("line_width")}
-        {parameterMap.get("nozzle_height")}*/}
-        {/*<input name='line_speed' value={inputValue} onChange={handleChange}/>*/}
+        <h1> Model </h1>
       </div>
+      <Canvas>
+        {/* <directionalLight position={[0,0,5]} />
+        <directionalLight position={[0,2,0]} /> */}
+        <directionalLight position={[5,2,2]} intensity={2} />
+        <hemisphereLight args={["#fff", "#333", 1]}/>
+        <ambientLight intensity={.5}/>
+
+        <Cube position={[0,-2,0]} color={"gray"} size={[1,1,1]}/>
+        <Cube position={[0,0,0]} color={"blue"} size={[1,1,1]}/>
+
+        <OrbitControls />
+      </Canvas>
+      
     </div>
   );
 };
