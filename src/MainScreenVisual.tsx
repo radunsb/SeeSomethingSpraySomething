1867931994b1fefabf763e0eb2d20457739b58f3
hemmerlinaj20 <100312616+hemmerlinaj20@users.noreply.github.com>
2025-01-22 21:47:18 -0500

    "Better" spraying system model. Learning more.

--- src/MainScreenVisual.tsx
+++ src/MainScreenVisual.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { UtilityInterfaces } from "./utility/models"
 import {Canvas} from "@react-three/fiber"
-import { OrbitControls } from "@react-three/drei"
+import { OrbitControls, Line } from "@react-three/drei"
+import * as THREE from 'three';
 
 const Cube = ({position, size, color}) => {
     return (
@@ -14,6 +15,9 @@ const Cube = ({position, size, color}) => {
 
 const MainScreenVisual = (parameterMap: Map<string, UtilityInterfaces.Parameter>, setParameterMap: Function) => {
 
+  const conveyor_piece_size = [12,.25,1.5]
+  const nozzle_size = [.5,.5,.5]
+
   // TODO: Get all of the valus from the current model that we need to build the visual
   const line_speed = parameterMap.get('line_speed').value;
   const line_width = parameterMap.get('line_width').value;
@@ -23,25 +27,91 @@ const MainScreenVisual = (parameterMap: Map<string, UtilityInterfaces.Parameter>
   // console.log(spray_angle);
 
   return (
-    <div>
-      <div>
-        <h1> Model </h1>
-      </div>
+    <div id='model_container'>
       <Canvas>
         {/* <directionalLight position={[0,0,5]} />
         <directionalLight position={[0,2,0]} /> */}
         <directionalLight position={[5,2,2]} intensity={2} />
         <hemisphereLight args={["#fff", "#333", 1]}/>
         <ambientLight intensity={.5}/>
+        
+        <group>
+          <Cube position={[0,-1,5.25]} color={"gray"} size={conveyor_piece_size}/>
+          <Cube position={[0,-1,3.5]} color={"gray"} size={conveyor_piece_size}/>
+          <Cube position={[0,-1,1.75]} color={"gray"} size={conveyor_piece_size}/>
+          <Cube position={[0,-1,0]} color={"gray"} size={conveyor_piece_size}/>
+          <Cube position={[0,-1,-1.75]} color={"gray"} size={conveyor_piece_size}/>
+          <Cube position={[0,-1,-3.5]} color={"gray"} size={conveyor_piece_size}/>
+          <Cube position={[0,-1,-5.25]} color={"gray"} size={conveyor_piece_size}/>
+          <Cube position={[0,-1,-7]} color={"gray"} size={conveyor_piece_size}/>
+          <Cube position={[0,-1,-8.75]} color={"gray"} size={conveyor_piece_size}/>
+          <Cube position={[0,-1,-10.5]} color={"gray"} size={conveyor_piece_size}/>
+        </group>
 
-        <Cube position={[0,-2,0]} color={"gray"} size={[1,1,1]}/>
-        <Cube position={[0,0,0]} color={"blue"} size={[1,1,1]}/>
+        <Cube position={[-2,3,0]} color={"black"} size={nozzle_size}/>
+        <Cube position={[2,3,0]} color={"black"} size={nozzle_size}/>
+
+        <Triangle vertices={[[-2, 3, 0],[-5, -1, 0],[1, -1, 0],]} width={1} color="blue" transparency={.3}/>
+        <Triangle vertices={[[2, 3, 0],[-1, -1, 0],[5, -1, 0],]} width={1} color="blue" transparency={.3}/>
+
+        {/* <Cube position={[0,0,0]} color={"blue"} size={[1,1,1]}/> */}
 
         <OrbitControls />
       </Canvas>
-      
     </div>
   );
 };
 
 export default MainScreenVisual;
+
+const Triangle = ({ vertices, width = 1, color = "#ffffff", transparency = 1 }) => {
+  if (!vertices || vertices.length !== 3) {
+    console.error("Triangle requires exactly 3 vertices.");
+    return null;
+  }
+
+  // Flatten the vertices array for the buffer attribute
+  const flattenedVertices = new Float32Array(vertices.flat());
+
+  return (
+    <group>
+      {/* Filled triangle */}
+      <mesh>
+        <bufferGeometry>
+          <bufferAttribute
+            attach="attributes-position"
+            array={flattenedVertices}
+            count={3}
+            itemSize={3}
+          />
+        </bufferGeometry>
+        <meshBasicMaterial
+          color={color}
+          side={THREE.DoubleSide}
+          transparent={true}
+          opacity={transparency}
+        />
+      </mesh>
+
+      {/* Outline (optional) */}
+      {width > 0 && (
+        <lineLoop>
+          <bufferGeometry>
+            <bufferAttribute
+              attach="attributes-position"
+              array={flattenedVertices}
+              count={3}
+              itemSize={3}
+            />
+          </bufferGeometry>
+          <lineBasicMaterial
+            color={color}
+            transparent={true}
+            opacity={transparency}
+            linewidth={width}
+          />
+        </lineLoop>
+      )}
+    </group>
+  );
+};
