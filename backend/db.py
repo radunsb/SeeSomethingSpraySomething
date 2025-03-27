4b82fa0a8a5d8394d8d7656b5d5801793a6aab88
radunsb <93169946+radunsb@users.noreply.github.com>
2025-03-27 11:41:16 -0400

    Fixed del['pass_hash'] issue

--- backend/db.py
+++ backend/db.py
@@ -33,14 +33,14 @@ def get_users():
 
 def get_user_by_id(user_id):
     try:
-        user = db.Users.find_one({'_id': user_id})
+        user = db.Users.find_one({'_id': user_id}, {'pass_hash':0})
         return user
     except Exception as e:
         return e
     
 def get_email(user_id):
     try:
-        user = db.Users.find_one({'_user': user_id})
+        user = db.Users.find_one({'_id': user_id}, {'pass_hash':0})
         return user['email']
     except Exception as e:
         return e
@@ -72,7 +72,7 @@ def get_user_recent_runs(user):
     
 def does_project_exist(user_id, pid):
     try:
-        user = db.Users.find_one({'_id': user_id})
+        user = db.Users.find_one({'_id': user_id}, {'pass_hash':0})
         projects = list(user['projects'])
         for project in projects:
             if(project['project_id'] == pid):
@@ -83,7 +83,7 @@ def does_project_exist(user_id, pid):
     
 def find_user_max_project_id(user_id):
     try:
-        user = db.Users.find_one({'_id': user_id})
+        user = db.Users.find_one({'_id': user_id}, {'pass_hash':0})
         projects = get_projects_by_user(user)
         if(not projects or len(projects) == 0):
             return 0
@@ -99,7 +99,7 @@ def save_new_project(user_id, projectJSON):
             {"_id": user_id},
             {"$push": {"projects": projectJSON}}
         )
-        user = db.Users.find_one({'_id': user_id})
+        user = db.Users.find_one({'_id': user_id}, {'pass_hash':0})
         return user
     except Exception as e:
         return e
@@ -110,7 +110,7 @@ def overwrite_existing_project(user_id, projectJSON):
             {"_id": user_id, "projects.project_id": projectJSON['project_id']},
             { "$set": { "projects.$" : projectJSON } }
         )
-        user = db.Users.find_one({'_id': user_id})
+        user = db.Users.find_one({'_id': user_id}, {'pass_hash':0})
         return user
     except Exception as e:
         return e
@@ -123,7 +123,7 @@ def delete_project(user_id, project_id):
             {"_id": user_id},
             {"$pull": {"projects": {"project_id": project_id}}}
         )
-        user = db.Users.find_one({'_id': user_id})
+        user = db.Users.find_one({'_id': user_id}, {'pass_hash':0})
         return user
     except Exception as e:
         return e   
@@ -132,7 +132,7 @@ def push_recent_project(user_id, project_id, projectJSON):
     try:
         if(project_id == 0):
             return None
-        user = db.Users.find_one({"_id": user_id})
+        user = db.Users.find_one({'_id': user_id}, {'pass_hash':0})
         if(len(user['recent_runs']) >= 30):
             db.Users.update_one(
                 {"_id": user_id},
@@ -149,7 +149,7 @@ def push_recent_project(user_id, project_id, projectJSON):
 
 def get_recents_for_this_project(user_id, project_id):
     try:
-        user = db.Users.find_one({"_id": user_id})
+        user = db.Users.find_one({'_id': user_id}, {'pass_hash':0})
         recents = get_user_recent_runs(user)
         recents = list(filter(lambda run: run['project_id'] == project_id, recents))
         return recents
